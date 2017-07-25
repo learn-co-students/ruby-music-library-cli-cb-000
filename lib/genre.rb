2da07de4e574cdfbcfd92c412ea10073b7e37ed0
theboymo <billfero@gmail.com>
2017-07-25 17:07:17 +0100

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,39 @@
+class Genre
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  # add song to the genre (has many), & genre to the song (belong to)
+  def add_song(song)
+    self.songs << song unless self.songs.include?(song)
+    song.genre = genre unless song.genre == self
+  end
+
+  # genre has many unique artists through songs
+  def artists
+    self.songs.collect {|song| song.artist}.uniq
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap {|g| g.save}
+  end
+end
