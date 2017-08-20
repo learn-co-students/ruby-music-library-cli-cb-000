f28dd5598b3659570ca701edbae1bf839217543b
Marcus Havranek <marcus.havranek@havranek.co.at>
2017-08-20 22:42:02 +0200

    startin with musicImporter

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,3 +1,21 @@
-
 class MusicImporter
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+
+  end
+
+  def self.new_from_filename(filename)
+
+  end
+
+  def self.create_from_filename
+
+  end
+
 end
