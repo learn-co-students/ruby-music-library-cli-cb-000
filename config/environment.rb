9c0aa3a922624250026a298f7f3884dcd26de155
Alex Segers <a.segers.dev@gmail.com>
2018-01-04 23:47:41 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,3 @@ module Concerns
 end
 
 require_all 'lib'
-=begin
-require './lib/concerns/concerns.rb'
-require './lib/artist.rb'
-require './lib/genre.rb'
-require './lib/song.rb'
-require './lib/music_importer.rb'
-=end
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,3 +1,4 @@
+
 class MusicLibraryController
 
   def initialize(path = './db/mp3s')
@@ -19,59 +20,79 @@ class MusicLibraryController
     input = gets.strip
     case input
       when 'list songs'
-
+        self.list_songs
       when 'list artists'
-
+        self.list_artists
       when 'list genres'
-
+        self.list_genres
       when 'list artist'
-
+        self.list_songs_by_artist
       when 'list genre'
-
+        self.list_songs_by_genre
       when 'play song'
-
+        self.play_song
     end
     self.call if input != 'exit'
 
   end #-- .call  --
 
   def list_songs
-    Song.all.sort.each do |song|
-      puts "#{Song.all.index(song) + 1}. #{song.artist} - #{song.name} - #{song.genre}"
+    sorted_songs = Song.all.sort_by {|song| song.name}
+    sorted_songs.each do |song|
+      puts "#{sorted_songs.index(song) + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
+    return "Success"
   end
 
   def list_artists
-    Artist.all.sort.each do |artist|
-      puts "#{Artist.all.index(song) + 1}. #{artist.name}"
+    sorted_artists = Artist.all.sort_by {|artist| artist.name}
+    sorted_artists.each do |artist|
+      puts "#{sorted_artists.index(artist) + 1}. #{artist.name}"
     end
+    return "Success"
   end
 
   def list_genres
-    Genre.all.sort.each do |genre|
-      puts "#{Genre.all.index(song) + 1}. #{genre.name}"
+    sorted_genres = Genre.all.sort_by {|genre| genre.name}
+    sorted_genres.each do |genre|
+      puts "#{sorted_genres.index(genre) + 1}. #{genre.name}"
     end
+    return "Success"
   end
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist_name = gets.strip
-    return nil if find_by_name(artist_name) == nil
+    return nil if Artist.find_by_name(artist_name) == nil
     artist = Artist.find_by_name(artist_name)
-    artist.songs.each do |song|
-      puts "#{artist.songs.index(song)}. #{song.name} - #{song.genre.name}"
+    sorted_songs = artist.songs.sort_by {|song| song.name}
+    sorted_songs.each do |song|
+      puts "#{sorted_songs.index(song) + 1}. #{song.name} - #{song.genre.name}"
     end
+    return "Success"
   end
 
   def list_songs_by_genre
-    puts "Please enter the name of an artist:"
+    puts "Please enter the name of a genre:"
     genre_name = gets.strip
-    return nil if find_by_name(artist_name) == nil
+    return nil if Genre.find_by_name(genre_name) == nil
     genre = Genre.find_by_name(genre_name)
-    genre.songs.each do |song|
-      puts "#{genre.songs.index(song)}. #{song.name} - #{song.artist.name}"
+    sorted_genres = genre.songs.sort_by {|song| song.name}
+    sorted_genres.each do |song|
+      puts "#{sorted_genres.index(song) + 1}. #{song.artist.name} - #{song.name}"
     end
+    return "Success"
   end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    sorted_songs = Song.all.sort_by {|song| song.name}
+    i = gets.strip.to_i
+    song = sorted_songs.detect {|song| sorted_songs.index(song) == i - 1}
+    return nil if i < 1 || i > sorted_songs.size
+    puts "Playing #{song.name} by #{song.artist.name}"
+  end
+
 
 
 end #-- MusicLibraryController  --
