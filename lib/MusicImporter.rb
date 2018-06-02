34298870c0cb02c56eb43339e1f0e6f1a196d783
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-06-02 04:42:43 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -12,3 +12,21 @@ class MusicImporter
     @files = @files.collect{|song| Song.create_from_filename(song)}
   end
 end
+
+# Teacher solution
+
+# class MusicImporter
+#   attr_reader :path
+#
+#   def initialize(path)
+#     @path = path
+#   end
+#
+#   def files
+#     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+#   end
+#
+#   def import
+#     files.each{ |f| Song.create_from_filename(f) }
+#   end
+# end
--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -16,3 +16,15 @@ module Concerns
   end
 
 end
+
+# Teacher solution
+
+# module Concerns::Findable
+#   def find_by_name(name)
+#     all.detect{ |o| o.name == name }
+#   end
+#
+#   def find_or_create_by_name(name)
+#     find_by_name(name) || create(name)
+#   end
+# end
