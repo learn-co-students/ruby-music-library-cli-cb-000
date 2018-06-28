f74ae0adc3ffba7c3be8092572014ee3d2904368
Adam Urbina <adamurbina@gmail.com>
2018-06-28 22:27:40 +0000

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,21 @@
+module Concerns
+    module Findable
+        def find_by_name(name)
+            match = nil
+            self.all.each {|item|
+            if item.name == name
+                match = item
+            end}
+            match
+        end
+
+        def find_or_create_by_name(name)
+            item = find_by_name(name)
+            if !item
+                item = self.create(name)
+            end
+            item
+        end
+
+    end
+end
--- lib/songs.rb
+++ lib/songs.rb
@@ -38,4 +38,41 @@ class Song
          name.songs << self if !name.songs.include?(self)
     end
 
+    def self.find_by_name(name)
+        match = nil
+        self.all.each{|song|
+        if song.name == name
+            match = song
+        end}
+        match
+    end
+
+    def self.find_or_create_by_name(name)
+        song = self.find_by_name(name)
+        if song == nil
+            song = self.create(name)
+        end
+        song
+    end
+
+    def self.new_from_filename(filename)
+        parsed = filename.split(" - ")
+        artist = parsed[0]
+        song_title = parsed[1]
+        genre = parsed[2].chomp(".mp3")
+        new_song = self.find_by_name(song_title)
+
+        if !new_song
+            new_artist = Artist.find_or_create_by_name(artist)
+            new_genre = Genre.find_or_create_by_name(genre)
+            new_song = Song.new(song_title, new_artist, new_genre)
+        end
+    end
+
+    def self.create_from_filename(filename)
+        song = new_from_filename(filename)
+        song.save
+        song
+    end
+
 end
