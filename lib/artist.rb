a92d92afa5fde9a26602e567b10486baff775a50
Tao <werewaswas@live.com>
2018-08-07 00:49:02 +0000

    1

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+  @@all = []
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    newInstance = self.new(name)
+    newInstance.save
+    newInstance
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist
+    @songs << song unless @songs.include?(song)
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end.uniq
+  end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,36 @@
+class Genre
+  @@all = []
+
+  attr_accessor :name
+  attr_reader :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    newInstance = self.new(name)
+    newInstance.save
+    newInstance
+  end
+
+  def artists
+    songs.map do |song|
+      song.artist
+    end.uniq
+  end
+
+end
