9e30f6e3285533c1d0c4401e4da08096ce00121c
Adam Mrachek <amrachek@outlook.com>
2019-08-21 17:08:46 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+  extend Concerns::Findable
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new_artist = Artist.new(name)
+    new_artist.save
+    new_artist
+  end
+
+  def add_song(song)
+    @songs << song unless @songs.include?(song)
+    song.artist = self unless song.artist
+  end
+
+  def genres
+    songs.map { |song| song.genre }.uniq
+  end
+end
